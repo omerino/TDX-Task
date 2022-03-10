c49e5144e3f8cf534fc6ad8ef11b3b66664ea03d
omerino <email2omer@gmail.com>
2022-03-11 00:17:55 +0200

    Update Task3.sh

--- Task3.sh
+++ Task3.sh
@@ -2,12 +2,12 @@
 if [ -d  ./Task3 ]; 
 then 
  
-		cp ./Task2/*.txt ./Task3 && chmod 444 ./Task3/*.txt
+		/cp ./Task2/*.txt ./Task3 && chmod 444 ./Task3/*.txt
 
 else 
 	mkdir ./Task3
 
-		cp ./Task2/*.txt ./Task3 && chmod 444 ./Task3/*.txt
+		/cp ./Task2/*.txt ./Task3 && chmod 444 ./Task3/*.txt
 fi
 
 
